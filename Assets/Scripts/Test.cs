1e4a016c81578027b8441999603f89de60aa9226
Haram Park <haram1117@khu.ac.kr>
2021-11-12 02:03:17 +0900

    PlayerState Updated

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -20,21 +20,21 @@ public class Test : MonoBehaviour //Just For Test
     {
         Player.Instance.loseMental(1);
     }
-    public void moneyup()
+
+    public void forceup50()
     {
-        Player.Instance.gainMoney(1);
+        Player.Instance.Changeability(player_ability.force, 50, 1);
     }
-    public void moneydown()
+    public void forcedown50()
     {
-        Player.Instance.loseMoney(1);
+        Player.Instance.Changeability(player_ability.force, 50, -1);
     }
+    //public void moneyup()
+    //{
+    //    Player.Instance.gainMoney(1);
+    //}
+    //public void moneydown()
+    //{
+    //    Player.Instance.loseMoney(1);
+    //}
 }
-/*
-[TO DO] 
-1. 업적
-2. 능력, 아이템, 상태 얻었을 떄 뜨는 메세지 구현
-3. 모험 재시작 구현
-4. pocket 창 옆 슬라이드로 넘어가게 & 카테고리 bold
-5. pocket 창 안 text 한줄에 세개 
-6. Die() 구현
- */
