f1f956334b88e805bd63a87bb9be3b39bdcf7b6f
mu08014 <81667084+mu08014@users.noreply.github.com>
2021-11-20 12:42:39 +0900

    acv and image update

--- Assets/Scripts/New Folder/AddText.cs
+++ Assets/Scripts/New Folder/AddText.cs
@@ -15,6 +15,10 @@ public class AddText : MonoBehaviour
     private Script script;
     private List<Choice> choice;
     private int num = 0;
+
+    public Image imgprefab;
+    private static List<Image> imgarr = new List<Image>();
+
     
     
     // Start is called before the first frame update
@@ -23,28 +27,44 @@ public class AddText : MonoBehaviour
         currentID = "M0_0";
         textBox = Instantiate(textPrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation);
         textBox.transform.SetParent(gameObject.transform, true);
-        textBox.transform.position += new Vector3(0, -40, 0);
+        textBox.transform.position = new Vector3(0, 0, 0);
         AddScript();
     }
 
     public void AddScript()
     {
         script = MakeDialog.Instance.FindScript(NextContainer.Instance.nextText);
-        if (currentID[0] == script.id[0] && currentID[1] == script.id[1]) // °°Àº Á¾·ù ½ºÅä¸®
+        if (currentID[0] == script.id[0] && currentID[1] == script.id[1]) // ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ä¸®
         {
             textBox.transform.position += new Vector3(0, 1400, 0);
             textBox.text += "\n\n\n\n\n\n\n\n\n\n\n\n\n";
             textBox.text += script.text;
             num++;
         }
-        else //»õ·Î¿î ½ºÅä¸®
+        else //ï¿½ï¿½ï¿½Î¿ï¿½ ï¿½ï¿½ï¿½ä¸®
         {
             textBox.transform.position += new Vector3(0, -1 * num * 1400, 0);
             textBox.text = script.text;
             num = 0;
+            DestroyPicture();
         }
         NextContainer.Instance.nextChoice = script.next;
         currentID = script.id;
     }
-    
+
+    public void AddPicture(Sprite picture, int n)          //n is changed by sprite size
+    {
+        Image img = Instantiate(imgprefab);
+        imgarr.Add(img);
+        img.sprite = picture;
+        img.transform.SetParent(textBox.transform, true);
+        img.transform.position = textBox.transform.position + new Vector3(0, n, 0);
+    }
+
+    public void DestroyPicture(){
+        for(int i = 0; i < imgarr.Count();i++){
+            Destroy(imgarr[i]);
+        }
+    }
+
 }
