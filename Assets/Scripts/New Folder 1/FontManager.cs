0c31769dede483e924f423b941f69af1d2b61c49
SHT4196 <SHT4196@gmail.com>
2022-01-25 00:12:02 +0900

    no message

--- Assets/Scripts/New Folder 1/FontManager.cs
+++ Assets/Scripts/New Folder 1/FontManager.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class FontManager : MonoBehaviour
 {
-    public Text Sample_Text;
-  
-   
+    public TextMeshProUGUI Sample_Text;
+    public TextMeshProUGUI storyText;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+       
     }
 
     // Update is called once per frame
@@ -22,43 +24,51 @@ public class FontManager : MonoBehaviour
     // 폰트 크게
     public void bigFontSize() 
     {
-        Sample_Text.fontSize = 100;
-        
+        Sample_Text.fontSize = 80;
+        storyText.fontSize = 80;
     }
 
     // 폰트 중간
     public void middleFontSize()
     {
-        Sample_Text.fontSize = 80;
-        
+        Sample_Text.fontSize = 70;
+        storyText.fontSize = 70;
+
     }
 
     // 폰트 작게
     public void smallFontSize()
     {
         Sample_Text.fontSize = 60;
-        
+        storyText.fontSize = 60;
+
     }
 
     // 줄간격 넓게
     public void wideLineSpace()
     {
-        Sample_Text.lineSpacing = 1.2f;
-       
+        Sample_Text.lineSpacing = 20f;
+        storyText.lineSpacing  = 20f;
+
+
     }
 
     // 줄간격 보통
     public void middleLineSpace()
     {
-        Sample_Text.lineSpacing = 1;
-        
+        Sample_Text.lineSpacing = 0f;
+        storyText.lineSpacing = 0f;
+
+
     }
 
     // 줄간격 좁게
     public void narrowLineSpace()
     {
-        Sample_Text.lineSpacing = 0.8f;
-        
+        Sample_Text.lineSpacing = -20f;
+        storyText.lineSpacing = -20f;
+
+
     }
 
 
