2b956a3af65f5380f141bed481efb7b7ed747756
SHT4196 <SHT4196@gmail.com>
2022-08-26 02:59:03 +0900

    오류 수정 및 해상도 조절

--- Assets/Scripts/AddText.cs
+++ Assets/Scripts/AddText.cs
@@ -78,50 +78,6 @@ public class AddText : MonoBehaviour
     {
         script = MakeDialog.instance.FindScript(NextContainer.instance.NextText);
         DestroyEmpty(); //Destroy empty text prefab 
-        textBox.Add(Instantiate(Resources.Load<TextMeshProUGUI>("Prefab/TextPrefab")));
-        textBox[textBox.Count-1].transform.SetParent(GameObject.Find("Content").transform , true);
-        textBox[textBox.Count - 1].rectTransform.localScale = new Vector3(widthRatio, heightRatio);
-
-        string coloredStr = "";
-        string value = script.text;
-        Debug.Log(value);
-
-        int indexNum = 0;
-        string[] val2 = value.Split('^');
-        if (val2.Length != 1)
-        {
-            indexNum = val2.Length;
-            for (int i = 0; i < indexNum; i++) {
-                if (i % 2 == 0) {
-                    coloredStr += val2[i];
-                }
-                else {
-                    coloredStr += "<color=#008000>";
-                    coloredStr += val2[i];
-                    coloredStr += "</color>";
-                }
-            }
-            script.text = coloredStr;
-        }
-
-        string[] val3 = script.text.Split('*');
-        string result = "";
-        if (val3.Length != 1)
-        {
-            indexNum = val3.Length;
-            for (int i = 0; i < indexNum; i++) {
-                if (i % 2 == 0) {
-                    result += val3[i];
-                }
-                else {
-                    result += "<color=#ff0000>";
-                    result += val3[i];
-                    result += "</color>";
-                }
-            }
-            script.text = result;
-        }
-        
         //float ScrollAmount = 0;
 
         if (script.acvUpdate.Count != 0 && PlayerPrefs.GetString("ScriptID") != script.id)
@@ -140,7 +96,9 @@ public class AddText : MonoBehaviour
             {
                 isimage.Add(false);
             }
-
+            textBox.Add(Instantiate(Resources.Load<TextMeshProUGUI>("Prefab/TextPrefab")));
+            textBox[textBox.Count - 1].transform.SetParent(GameObject.Find("Content").transform, true);
+            textBox[textBox.Count - 1].rectTransform.localScale = new Vector3(widthRatio, heightRatio);
             if (!Player.instance.isAdmin)
             {
                 PlayerPrefs.SetString("ScriptID", script.id); //save id
@@ -214,6 +172,53 @@ public class AddText : MonoBehaviour
         }
         NextContainer.instance.NextChoice = script.next;
         currentID = script.id;
+
+
+        string coloredStr = "";
+        string value = script.text;
+        Debug.Log(value);
+
+        int indexNum = 0;
+        string[] val2 = value.Split('^');
+        if (val2.Length != 1)
+        {
+            indexNum = val2.Length;
+            for (int i = 0; i < indexNum; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    coloredStr += val2[i];
+                }
+                else
+                {
+                    coloredStr += "<color=#008000>";
+                    coloredStr += val2[i];
+                    coloredStr += "</color>";
+                }
+            }
+            script.text = coloredStr;
+        }
+
+        string[] val3 = script.text.Split('*');
+        string result = "";
+        if (val3.Length != 1)
+        {
+            indexNum = val3.Length;
+            for (int i = 0; i < indexNum; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    result += val3[i];
+                }
+                else
+                {
+                    result += "<color=#ff0000>";
+                    result += val3[i];
+                    result += "</color>";
+                }
+            }
+            script.text = result;
+        }
     }
 
     /// <summary>
