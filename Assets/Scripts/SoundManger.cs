44c505ded4db1de7e9d2f06521fc7684fbf6518f
doyon <doyon3x3@gmail.com>
2022-12-18 03:04:59 +0900

    effect sound slider & add btn sound

--- Assets/Scripts/SoundManger.cs
+++ Assets/Scripts/SoundManger.cs
@@ -7,24 +7,41 @@ using UnityEngine.UI;
 public class SoundManger : MonoBehaviour
 {
 
-    
     public AudioMixer masterMixer;
     public Slider BgmSlider;
+    public Slider EffectSlider;
+
+    public AudioSource audioSource;
+    public AudioClip ClickSound;
+    public static SoundManger instance;
 
     /// <summary>
     /// ���� ����
     /// </summary>
 
+    void Awake()
+    {
+        if (SoundManger.instance == null)
+        {
+            SoundManger.instance = this;
+        }
+    }
+
     void Start()
     {
-        if (PlayerPrefs.HasKey("volume"))
+        if (PlayerPrefs.HasKey("BGMvolume"))
         {
-            BgmSlider.value = PlayerPrefs.GetFloat("volume");
+            BgmSlider.value = PlayerPrefs.GetFloat("BGMvolume");
+            // masterMixer.setFloat("BGM", BgmSlider.value);
+        }
+        if (PlayerPrefs.HasKey("Effectvolume"))
+        {
+            BgmSlider.value = PlayerPrefs.GetFloat("Effectvolume");
             // masterMixer.setFloat("BGM", BgmSlider.value);
         }
     }
 
-    public void AudioControl()
+    public void BGMAudioControl()
     {
         float sound = BgmSlider.value;
 
@@ -32,9 +49,24 @@ public class SoundManger : MonoBehaviour
         else
         {
             masterMixer.SetFloat("BGM", sound);
-            PlayerPrefs.SetFloat("volume", sound);
+            PlayerPrefs.SetFloat("BGMvolume", sound);
+        }
+    }
+    public void EffectAudioControl()
+    {
+        float sound = EffectSlider.value;
+
+        if (sound == -40f) masterMixer.SetFloat("Effect", -80);
+        else
+        {
+            masterMixer.SetFloat("Effect", sound);
+            PlayerPrefs.SetFloat("EffectVolume", sound);
         }
     }
 
+    public void PlayBtnSound()
+    {
+        audioSource.PlayOneShot(ClickSound);
+    }
 
 }
