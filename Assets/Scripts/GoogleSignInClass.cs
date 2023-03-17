3dd498bfd1c9937087b414382d84f7d9c9cf5fb9
SHT4196 <SHT4196@gmail.com>
2023-03-17 21:51:16 +0900

    no message

--- Assets/Scripts/GoogleSignInClass.cs
+++ Assets/Scripts/GoogleSignInClass.cs
@@ -32,6 +32,42 @@ public class GoogleSignInClass : MonoBehaviour
             IsEditor = true;
     }
 
+    Firebase.Auth.FirebaseUser user;
+
+    // Handle initialization of the necessary firebase modules:
+    void InitializeFirebase()
+    {
+        Debug.Log("Setting up Firebase Auth");
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.StateChanged += AuthStateChanged;
+        AuthStateChanged(this, null);
+    }
+
+    // Track state changes of the auth object.
+    void AuthStateChanged(object sender, System.EventArgs eventArgs)
+    {
+        if (auth.CurrentUser != user)
+        {
+            bool signedIn = user != auth.CurrentUser && auth.CurrentUser != null;
+            if (!signedIn && user != null)
+            {
+                Debug.Log("Signed out " + user.UserId);
+            }
+            user = auth.CurrentUser;
+            if (signedIn)
+            {
+                Debug.Log("Signed in " + user.UserId);
+            }
+        }
+    }
+
+    void OnDestroy()
+    {
+        auth.StateChanged -= AuthStateChanged;
+        auth = null;
+    }
+
+
     private void CheckFirebaseDependencies()
     {
         FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
@@ -178,5 +214,7 @@ public class GoogleSignInClass : MonoBehaviour
     private void AddToInformation(string str) { infoText.text += "\n" + str; }
 
     
+  
+
 
 }
